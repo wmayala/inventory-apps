161cab29b970f871809d5a993fef8c59f6195b8a
wmayala <wma.dev5@gmail.com>
2023-07-13 15:31:15 -0600

    update

--- inventoryApps/inventoryApps/urls.py
+++ inventoryApps/inventoryApps/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for inventarioApps project.
+URL configuration for inventoryApps project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
