200284e09760ba4a0d823c0de7cc99f3276ee7db
wmayala <wma.dev5@gmail.com>
2023-07-14 16:30:10 -0600

    Add add_app template

--- inventoryApps/inventory/urls.py
+++ inventoryApps/inventory/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', login_required(views.index), name = 'index'),
     path('login', views.mylogin, name = 'registration.login'),
     path('logout', views.mylogout, name='registration.logout'),
+    
+    path('add', login_required(views.add_app), name = 'add_app')
 ]
--- inventoryApps/inventory/views.py
+++ inventoryApps/inventory/views.py
@@ -21,37 +21,18 @@ def mylogin(request):
             user = authenticate(username = login_form.cleaned_data['username'], password = login_form.cleaned_data['password'])
             login(request, user)
             messages.success(request, '¡Ingreso exitoso!')
-            #login_form.add_error(None, 'Ingreso exitoso')
             return redirect('index')
         else:
             messages.warning(request, 'Credenciales inválidas')
-            #login_form.add_error(None, 'Credenciales inválidas')
     data['form'] = login_form
-    
     return render(request, template_name, data)
-    
-    """ if request.method == 'POST':
-        form = MyLoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            
-            print(user)
-            
-            if user:
-                login(request, user)
-                return redirect('index')  # Redirige a la página de inicio después del inicio de sesión exitoso
-            else:
-                form.add_error(None, 'Credenciales inválidas')
-    else:
-        form = MyLoginForm()
-    
-    return render(request, 'registration/login.html', {'form': form}) """
 
 def mylogout(request):
     logout(request)
     return redirect('registration.login')
 
 def index(request):
-    return render(request, template_name='index.html')
+    return render(request, 'index.html')
+
+def add_app(request):
+    return render(request, 'application/add.html')
